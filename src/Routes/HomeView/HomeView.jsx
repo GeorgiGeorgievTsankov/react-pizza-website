019b760935eb2fd2c49c30c3e313d8580efc3513
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-03 14:56:15 +0300

    Update HomeView.jsx

--- src/Routes/HomeView/HomeView.jsx
+++ src/Routes/HomeView/HomeView.jsx
@@ -5,21 +5,9 @@ import {ButtonComponent} from '../../components/Button/ButtonComponent';
 
 export const HomeView = () => {
 
-  const clickMeRed = () => { 
-    console.log("red");
-  }
-  
-  const clickMeBlue = () => {
-    console.log("blue");
-  }
-
-
-
   return (
     <div>
       <h1>Pizza</h1>
-      <ButtonComponent onClick={clickMeRed} className="button-red">Click</ButtonComponent>
-      <ButtonComponent onClick={clickMeBlue} className="button-blue">Click</ButtonComponent>
     </div>
   )
 }
