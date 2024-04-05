9cd0e059320e84ef83e0526adc668f49dd1f05ec
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-05 14:09:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-import { HomeView } from './Routes/HomeView/HomeView'
-import { NavbarComponent } from './components/Navbar/NavbarComponent'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomeView } from './Routes/HomeView/HomeView';
+import { NavbarComponent } from './components/Navbar/NavbarComponent';
+import { AboutView } from './Routes/About/AboutView';
 
 export const App = () => {
   return (
     <Router>
       <NavbarComponent />
       <Routes>
-        <Route path="/" element={HomeView} />
+        <Route path="/" element={<HomeView />} /> 
+        <Route path="/about" element={<AboutView />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
