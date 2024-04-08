77f6869d1b42f70d6a5e09c368cfcb8eb8119d32
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-08 15:47:17 +0300

    Update HomeView.jsx

--- src/Routes/HomeView/HomeView.jsx
+++ src/Routes/HomeView/HomeView.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
 
-export const HomeView = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../../components/Assets/pizza.jpeg";
+import "./HomeViewStyle.css";
 
+export function HomeView() {
   return (
-    <div>
-      <h1>Pizza</h1>
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> Pedro's Pizzeria </h1>
+        <p> PIZZA TO FIT ANY TASTE</p>
+        <Link to="/menu">
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
