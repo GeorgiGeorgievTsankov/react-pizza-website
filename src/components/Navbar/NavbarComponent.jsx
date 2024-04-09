00265b2bf22821b76c88c1165303a4d3176a40e0
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-09 09:49:04 +0300

    Update NavbarComponent.jsx

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -14,7 +14,7 @@ export function NavbarComponent() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <Link to="/"> <img src={Logo} /> </Link>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
