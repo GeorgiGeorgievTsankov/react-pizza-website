8a2bfa98817cc893274c1b18143dd99922050141
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-09 09:22:29 +0300

    Add files via upload

---
+++ src/Routes/Menu/MenuView.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { MenuList } from "../../components/Helpers/MenuList";
+import { MenuItem } from "../../components/MenuItem/MenuItem";
+import "./MenuStyle.css";
+
+export function MenuView() {
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+        {MenuList.map((menuItem, key) => {
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
