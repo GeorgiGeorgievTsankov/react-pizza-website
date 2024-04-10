8ec5d920b7ddaa3fb1d138266e928d2ec3f2a62c
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-10 18:30:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomeView } from './Routes/HomeView/HomeView';
 import { NavbarComponent } from './components/Navbar/NavbarComponent';
 import { AboutView } from './Routes/About/AboutView';
+import { Footer } from './components/Footer/Footer';
+import { MenuView } from './Routes/Menu/MenuView';
+import { ContactsView } from './Routes/Contacts/ContactsView';
+import './App.css'
 
 export const App = () => {
   return (
@@ -10,7 +14,10 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<HomeView />} /> 
         <Route path="/about" element={<AboutView />} />
+        <Route path="/menu" element={<MenuView />} /> 
+        <Route path="/contact" element={<ContactsView />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
