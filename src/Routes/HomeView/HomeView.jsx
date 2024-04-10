c4c7b5476c8666a20c379298b604f7046d5644c1
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-10 18:22:23 +0300

    Update HomeView.jsx

--- src/Routes/HomeView/HomeView.jsx
+++ src/Routes/HomeView/HomeView.jsx
@@ -1,17 +1,17 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../../components/Assets/pizza.jpeg";
+import BannerImage from "../../components/Assets/multiplePizzas.jpeg";
+import { ButtonComponent } from "../../components/Button/ButtonComponent";
 import "./HomeViewStyle.css";
 
 export function HomeView() {
   return (
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <h1> Pedro's Pizzeria </h1>
+        <h1> Tsankov Pizzeria </h1>
         <p> PIZZA TO FIT ANY TASTE</p>
-        <Link to="/menu">
-          <button> ORDER NOW </button>
+        <Link to="/menu" className="button-home">
+          <ButtonComponent className="button-red">ORDER NOW</ButtonComponent>
         </Link>
       </div>
     </div>
