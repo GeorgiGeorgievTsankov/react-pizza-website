15d37d3da685525fb6258e609a945e52a4f72054
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-04-13 17:55:14 +0300

    Update NavbarComponent.jsx

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -5,32 +5,32 @@ import MenuIcon from '@mui/icons-material/Menu';
 import "./Navbar.css";
 
 export function NavbarComponent() {
-  const [openLinks, setOpenLinks] = useState(false);
+    const [openLinks, setOpenLinks] = useState(false);
 
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
-  };
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+    };
 
-  return (
-    <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <Link to="/"> <img src={Logo} /> </Link>
-        <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/contact"> Contact </Link>
+    return (
+        <div className="navbar">
+            <div className="leftSide" id={openLinks ? "open" : "close"}>       
+                <Link to="/"> <img src={Logo} /> </Link>
+                <div className="hiddenLinks">
+                    <Link to="/"> Home </Link>
+                    <Link to="/menu"> Menu </Link>
+                    <Link to="/about"> About </Link>
+                    <Link to="/contact"> Contact </Link>
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to="/"> Home </Link>
+                <Link to="/menu"> Menu </Link>
+                <Link to="/about"> About </Link>
+                <Link to="/contact"> Contact </Link>
+                <button onClick={toggleNavbar}>
+                    <MenuIcon />
+                </button>
+            </div>
         </div>
-      </div>
-      <div className="rightSide">
-        <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/contact"> Contact </Link>
-        <button onClick={toggleNavbar}>
-          <MenuIcon />
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
